983635d2010eeca1a9c0d71fb7cfba3e7ce3d995
diego-ciciani01 <diego.ciciani@gmail.com>
2026-07-14 10:19:45 +0200

    refactor into fixpoint driver

--- src/MyDCE.cpp
+++ src/MyDCE.cpp
@@ -15,11 +15,65 @@ using namespace llvm;
  * This function is used to verify if the istruction is basicaly dead
  * 'mayHaveSideEffects()' -> this single method can cover: volatile operations and atomic
  */
-bool isTriviallyDead(Instruction &I){
+static bool isTriviallyDead(Instruction &I){
   if (I.use_empty() && !I.isTerminator() && !I.mayHaveSideEffects())
     return true;
 
   return false;
+}
+
+static bool deadCodeElimination(Function &F){
+  llvm::SmallVector<Instruction *, 64> worklist;
+  bool changed = false; /* Used to control the type of return */
+      
+  /* SEED phase: check the already dead and fill the stack. */
+  for (BasicBlock &BB : F){
+    for (Instruction &I : BB)
+      if (isTriviallyDead(I))
+	worklist.push_back(&I);	
+  }
+  
+  /*
+   * The apporach used is the worklist.
+   * Means save in this stak each eligible dead instrcution.
+   * WorkList approch has invariant property: every instruction enters the worklist at most once.
+   *
+   */
+  while(!worklist.empty()){
+    /* Get back the value from stack */
+    Instruction *I = worklist.pop_back_val();
+    SmallVector<Instruction *, 4> operands;
+	
+    /* 'users' handle the pointer to the operands of isturction, that we're processing. */
+    for (Use &Op: I->operands()){
+      /* Here we want to check if the operands are "pointers" eligitable, the if statement will return
+       * true, otherwise false.
+       * Eg.  mul nsw i32 %add, 2 
+       */
+      if (Instruction *OptInst =  dyn_cast<Instruction>(Op.get()))
+	operands.push_back(OptInst);
+    }
+
+    /* Deallocate the current "I" istructtion from graph and free the memory */
+    I->eraseFromParent();
+    changed = true;
+	
+    /* Now since I it's been eliminated, it's operands could be become dead too*/
+    for (Instruction *O : operands){
+      if (isTriviallyDead(*O))
+	worklist.push_back(O);  
+    }
+	
+  }
+
+  return changed;
+  
+}
+
+/* Folding function to semplify a "complex" with an easier one*/
+static bool foldSameTarghetBranches(Function &F){
+
+
 }
 
 namespace {
@@ -27,55 +81,16 @@ namespace {
   struct MyDCEPass: PassInfoMixin<MyDCEPass>{
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM){
-      llvm::SmallVector<Instruction *, 64> worklist;
-      bool changed = false; /* Used to control the type of return */
-      
-      /* SEED phase: check the already dead and fill the stack. */
-      for (BasicBlock &BB : F){
-	for (Instruction &I : BB)
-	  if (isTriviallyDead(I))
-	    worklist.push_back(&I);	
-      }
+      bool changed ;
+      bool allChanged = false;
+      /* Main loop */
+      do{
+	changed = deadCodeElimination(F);
+	allChanged != 
+      }while(changed);
 
-      /*
-       * The apporach used is the worklist.
-       * Means save in this stak each eligible dead instrcution.
-       * WorkList approch has invariant property: every instruction enters the worklist at most once.
-       *
-       */
-
-      while(!worklist.empty()){
-	/* Get back the value from stack */
-	Instruction *I = worklist.pop_back_val();
-	SmallVector<Instruction *, 4> operands;
 	
-	/* 'users' handle the pointer to the operands of isturction, that we're processing. */
-	for (Use &user : I->operands()){
-	  /* Here we want to check if the operands are "pointers" eligitable, the if statement will return
-	   * true, otherwise false.
-	   * Eg.  mul nsw i32 %add, 2 
-	   */
-	  if (Instruction *Option =  dyn_cast<Instruction>(user.get())){
-	    if (Option != nullptr)
-	      operands.push_back(Option);
-	  }
-	}
-
-	/* Deallocate the current I istructtion from graph and free the memory */
-	I->eraseFromParent();
-	changed = true;
-	
-	/* Now since I it's been eliminated, it's operands could be become dead too*/
-	for (Instruction *O : operands){
-	  if (isTriviallyDead(*O)){ /* Check also if is not present in the worklist*/
-	      worklist.push_back(O);
-	    
-	  }
-	  
-	}
-	
-      }
-	
+           
       return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
     }
 
