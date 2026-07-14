d543d8615827ec3a4adcd8c27359bd6f4fc14d23
diego-ciciani01 <diego.ciciani@gmail.com>
2026-07-14 19:53:33 +0200

    implement unreachable block removal, block bypassing and branch folding
    Added three key control flow graph (CFG) simplification passes to optimize the control flow of functions:
    
    - Integrated  to clean up and delete
      any basic blocks that cannot be reached from the entry point.
    - Implemented bypassBlock(F) to safely reroute predecessors of empty
      blocks (containing only an unconditional branch) directly to their
      successor. Included a strict safety check (>phis().empty()) to
      bail out and prevent SSA violations.
    - Added branch folding logic (foldSameTargetBranches) to simplify and
      clean up redundant branch destinations that target the same block.

--- src/MyDCE.cpp
+++ src/MyDCE.cpp
@@ -1,11 +1,12 @@
+
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/raw_ostream.h"
 
 #if __has_include("llvm/Plugins/PassPlugin.h")
-#include "llvm/Plugins/PassPlugin.h"   // LLVM >= 22
+#include "llvm/Plugins/PassPlugin.h"   /* LLVM >= 22 */
 #else
-#include "llvm/Passes/PassPlugin.h"    // LLVM <= 21
+#include "llvm/Passes/PassPlugin.h"    /* LLVM <= 21 */
 #endif
 
 /* Requred to find the 'include' files from llvm namespace*/
@@ -37,7 +38,6 @@ static bool deadCodeElimination(Function &F){
    * The apporach used is the worklist.
    * Means save in this stak each eligible dead instrcution.
    * WorkList approch has invariant property: every instruction enters the worklist at most once.
-   *
    */
   while(!worklist.empty()){
     /* Get back the value from stack */
@@ -71,30 +71,168 @@ static bool deadCodeElimination(Function &F){
 }
 
 /* Folding function to semplify a "complex" with an easier one*/
-static bool foldSameTarghetBranches(Function &F){
+static bool foldSameTargetBranches(Function &F){
+  bool changed = false;
+  
+  for (BasicBlock &BB : F){
+    if (Instruction *I =  BB.getTerminator()){
 
+      if (BranchInst *BI = dyn_cast<BranchInst>(I)){
+	/* Check the case in with branch has the same destination*/
+	if (BI->isConditional() && BI->getSuccessor(0) == BI->getSuccessor(1)){
+	  if (!BI->getSuccessor(0)->phis().empty())
+	    continue;
+	  /* now create the new "br"*/
+	  BranchInst::Create( BI->getSuccessor(0), BI->getIterator());
+	  /* Delete the old incoditional branch */
+	  BI->eraseFromParent();
+	  changed = true;
+	}
+	
+      }
 
+    }
+      
+  }
+
+  return changed;
+}
+
+static bool removeUnreachableBlocksLocal(Function &F){
+ 
+  SmallVector<BasicBlock*, 16> stack; /* stack with, to delete blocks */
+  SmallPtrSet<BasicBlock*, 16> checked; /* set with all 'healty' block */
+  
+  /* hint "getEntryBlock" get back the first block of the function! */
+  for (BasicBlock *BB : depth_first(&F.getEntryBlock())) 
+    checked.insert(BB);
+  
+  /* Now check all the nodes in the set, that are not reachable */
+  for (BasicBlock &BB : F)
+    if (!checked.count(&BB))
+      stack.push_back(&BB);
+
+  /* In the case in witch all the block are not reachable, exit*/
+  if (stack.empty())
+    return false;
+
+  
+  /* Remove all the link between the then and cleaning */
+  for (BasicBlock *BB : stack){
+
+    for (BasicBlock *suc : successors(BB)){
+      /* Situation in witch the successor is an haltly block */
+      if (checked.count(suc)){ 
+	for (PHINode &PN : suc->phis()){
+	  /* Remove the ingress associated with BB
+	   * False params, avoid to delete the phi with one ingress. It's will be eliminated in the next loop, by DCE. */
+	  PN.removeIncomingValue(BB, false);
+	}
+      }
+    }
+    BB->dropAllReferences();
+  }
+
+  /*Phisical elimination memory */
+
+  for (BasicBlock *BB : stack)
+    BB->eraseFromParent();
+  
+  return true;
+    
+}
+
+/*
+ * I decided to avoid bypassing blocks whose successor contains PHI node.
+ * Correctly updating the SSA form require additional handling of duplicate incoming edges. 
+ */
+
+static bool bypassBlock(Function &F){
+  bool changed = false;
+  
+  for (BasicBlock &BB : F){
+    /* Check if the block contains only the terminator istruction
+     * Uncoditional branch, and eventualy PHI
+    */
+    if (BB.size() != 1)
+      continue;
+
+    /* Here there is not previus block ... */
+    if (&BB == &F.getEntryBlock())
+      continue;
+
+    
+    /*Get last istruction of block, and cast it in "BranchInst" */
+    BranchInst *BI = dyn_cast<BranchInst>(BB.getTerminator());
+     /* */
+    if (!BI || !BI->isUnconditional())
+      continue;
+
+    /* Successor of the unconditional branch */
+    BasicBlock *suc = BI->getSuccessor(0);
+    
+    /* Check self loop*/
+    if (suc == &BB)
+      continue;
+
+    /* bail-out to prevent problem with SSA semantic
+     * if the successor has PHI node
+     */
+    if (!suc->phis().empty())
+      continue;
+
+    SmallVector<BasicBlock *, 16> preds(predecessors(&BB));
+    
+    /* update the PHI of successor to registrate the new predecessor. */
+    for (PHINode &PN : suc->phis()){
+      int indexBlock = PN.getBasicBlockIndex(&BB);
+
+      if (indexBlock < 0)
+	continue; 
+
+      Value *V = PN.getIncomingValue(indexBlock);
+
+      PN.removeIncomingValue(indexBlock, false);
+	
+      for (BasicBlock *pred : preds)
+	PN.addIncoming(V, pred);
+      
+    }
+
+    /* Redirect predecessors */
+    for (BasicBlock *pred : preds)
+      pred->getTerminator()->replaceSuccessorWith(&BB, suc);
+
+    changed = true;
+  }
+  
+  return changed;
 }
 
 namespace {
-  /* 'PassInfoMixin': is a required pass, with CRTP pattern */
+  /* 'PassInfoMixin': is a required pass, with CRTP pattern. */
   struct MyDCEPass: PassInfoMixin<MyDCEPass>{
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM){
-      bool changed ;
-      bool allChanged = false;
+      bool changed = false ;
+      bool localyChanged = false;
       /* Main loop */
       do{
 	changed = deadCodeElimination(F);
-	allChanged != 
+	changed |= foldSameTargetBranches(F);
+	changed |= removeUnreachableBlocksLocal(F);
+	changed |= bypassBlock(F);
+	
+	if(changed)
+	  localyChanged = true;
       }while(changed);
 
-	
-           
-      return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
+                  
+      return localyChanged ? PreservedAnalyses::none() : PreservedAnalyses::all();
     }
-
+    static bool isRequired() { return true; }
   };
+  
 }
 
 
@@ -106,7 +244,7 @@ extern "C" LLVM_ATTRIBUTE_WEAK  PassPluginLibraryInfo llvmGetPassPluginInfo() {
                    ArrayRef<PassBuilder::PipelineElement>) {
                   if (Name == "my-dce") {
                     FPM.addPass(MyDCEPass());
-                    return true;   // name found
+                    return true;  
                   }
                   return false;    
                 });
